71a5b8704930c8e9c4552e0ec5cd72fd94e5070e
Matthew Koontz <mjk3979@rit.edu>
2013-10-31 21:13:31 -0400

    Can now extract the day that a class is held.

--- extractor.py
+++ extractor.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+def getDay(entry):
+	parent = entry.find_parent(attrs={'class' : 'SSSWEEKLYBACKGROUND'})
+	index = -1
+	for _ in parent.previous_siblings:
+		index += 1
+	return DAYS[index]
+
+def getClasses(soup):
+	soup.prettify()
+	for entry in soup.find_all(attrs={'class': 'SSSTEXTWEEKLY'}):
+		print(getDay(entry))
+		print('\t'.join([s for s in entry.strings]))
+
 soup = BeautifulSoup(open('test.html', 'r').read())
-soup.prettify()
-for entry in soup.find_all(attrs={'class': 'SSSTEXTWEEKLY'}):
-	print('\t'.join([s for s in entry.strings]))
+getClasses(soup)
